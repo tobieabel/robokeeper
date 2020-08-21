269f0a844c2d410ed417a2718bc61b96587b006f
tobieabel <tobieabel@gmail.com>
2020-08-21 15:51:11 +0100

    32

--- TFLite_detection_Staticwebcamv3.py
+++ TFLite_detection_Staticwebcamv3.py
@@ -1,7 +1,7 @@
 ######## Webcam Object Detection Using Tensorflow-trained Classifier #########
 #
 # Author: Tobie Abels
-# Date: 31/8/2020
+# Date: 32/8/2020
 # Description:
 # This program uses a TensorFlow Lite model to perform object detection on a live webcam
 # feed. It draws boxes and scores around the objects of interest in each frame from the
